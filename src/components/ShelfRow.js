69777d2bcbe05f6f9a6091a450bc80e96408104d
Robert Chung <chungrob@gmail.com>
2018-07-24 12:59:15 -0500

    Full Shelf

--- src/components/ShelfRow.js
+++ src/components/ShelfRow.js
@@ -4,7 +4,7 @@ import Shoe from './Shoe.js';
 
 const ShelfRow = (props) => (
   <tr>
-    <td>Row {props.row.row}</td>
+    <td>Row: {props.row.row}</td>
     { props.row.shoes.map((shoe) => <Shoe key={shoe.id} shoe={shoe}/> )}
   </tr>
 )
--- src/components/Shoe.js
+++ src/components/Shoe.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const Shoe = (props) => (
-  <td>
-    ID: { props.shoe.id } <br />  
-    Brand: { props.shoe.brand } <br />
-    Style: { props.shoe.style } <br />
-    Size: { props.shoe.size } <br />
-    UPC: { props.shoe.UPC } <br />
-    {/* Img: {props.shoe.imgLink} */}
+  <td class="container">
+    <img src={props.shoe.imgLink} alt="Shoe" class="shoe"/>
+    <div class = "overlay">
+        <div class = "shoe-info">
+            ID: { props.shoe.id } <br />  
+            Brand: { props.shoe.brand } <br />
+            Style: { props.shoe.style } <br />
+            Size: { props.shoe.size } <br />
+            UPC: { props.shoe.UPC } <br />
+            {/* Img: {props.shoe.imgLink} */}
+        </div>
+    </div>
   </td>
 )
 export default Shoe;
